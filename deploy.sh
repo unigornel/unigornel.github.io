907cfa0dd4e3da1abc5d139269ea74f0a00800cc
Henri Verroken <henriverroken@gmail.com>
2016-06-29 11:25:33 +0200

    rebuilding site Wed Jun 29 11:25:33 CEST 2016

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,4 @@ fi
 git commit -m "$msg"
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:unigornel/unigornel.org.git gh-pages
+git subtree push -f --prefix=public git@github.com:unigornel/unigornel.org.git gh-pages
